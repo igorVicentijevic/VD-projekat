1f89267bfaa6ee66981c17ede230dbba16fc80dd
igorVicentijevic <153335922+igorVicentijevic@users.noreply.github.com>
2024-09-07 12:26:49 +0200

    Add files via upload

---
+++ projekat_final/Data/cene.js
@@ -0,0 +1,50 @@
+//AKO SE MENJA NASLOV OBAVEZNO PROMENITI OVDE IME
+const cene= [
+    {
+        ime: "Pohovana banana sa miks prelivom",
+        velikaPorcija: 600,
+        malaPorcija: 450
+    },
+    {
+        ime: "Ananas sa slagom i prelivom od sumskog voca",
+        velikaPorcija: 650,
+        malaPorcija: 550
+    },
+    {
+        ime: "Kineski kolacici",
+        velikaPorcija: 500,
+        malaPorcija: 350
+    },
+    {
+        ime: "Piletina sa sampinjonima",
+        velikaPorcija: 850,
+        malaPorcija: 600
+    },
+    {
+        ime: "Svinjetina sa karijem i nudlama",
+        velikaPorcija: 950,
+        malaPorcija: 700
+    },
+    {
+        ime: "Biftek u sosu od ostriga",
+        velikaPorcija: 1700,
+        malaPorcija: 1400
+    },
+    {
+        ime: "Zacinjena svinjetina",
+        velikaPorcija: 1000,
+        malaPorcija: 800
+    },
+    {
+        ime: "Zacinjeni krompir",
+        velikaPorcija: 550,
+        malaPorcija: 350
+    },
+    {
+        ime: "Nudle u sosu od povrca",
+        velikaPorcija: 600,
+        malaPorcija: 400
+    }
+    
+]
+
---
+++ projekat_final/Data/ceneEN.js
@@ -0,0 +1,47 @@
+const ceneEN = [
+    {
+        ime: "Breaded banana with mixed sauce",
+        velikaPorcija: 600,
+        malaPorcija: 450
+    },
+    {
+        ime: "Pineapple with cream and wild fruit sauce",
+        velikaPorcija: 650,
+        malaPorcija: 550
+    },
+    {
+        ime: "Chinese cookies",
+        velikaPorcija: 500,
+        malaPorcija: 350
+    },
+    {
+        ime: "Chicken with mushrooms",
+        velikaPorcija: 850,
+        malaPorcija: 600
+    },
+    {
+        ime: "Pork with curry and noodles",
+        velikaPorcija: 950,
+        malaPorcija: 700
+    },
+    {
+        ime: "Beef steak in oyster sauce",
+        velikaPorcija: 1700,
+        malaPorcija: 1400
+    },
+    {
+        ime: "Spiced pork",
+        velikaPorcija: 1000,
+        malaPorcija: 800
+    },
+    {
+        ime: "Seasoned potatoes",
+        velikaPorcija: 550,
+        malaPorcija: 350
+    },
+    {
+        ime: "Noodles in vegetable sauce",
+        velikaPorcija: 600,
+        malaPorcija: 400
+    }
+];
